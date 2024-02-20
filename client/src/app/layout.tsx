568777e9a1250be7b724476ca41060f8be4e6258
Hansen, Sivert Gullberg <sivert-gullberg.hansen@norsk-tipping.no>
2024-02-20 23:11:09 +0100

    added icon

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,11 +2,12 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import NextAuthSessionProvider from "@/providers/SessionProvider";
 import { Navbar } from "@/components/Navbar/Navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Brackets",
+  title: "Tournament",
 };
 
 export default function RootLayout({
@@ -16,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html className="bg-black" lang="en">
+      <head>
+        <link rel="icon" href="/logo.svg" sizes="any" />
+      </head>
       <body className={`${inter.className} w-screen h-screen`}>
         <NextAuthSessionProvider>
           <main className="flex flex-col w-full h-full">
