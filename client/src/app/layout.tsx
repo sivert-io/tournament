299db2538e3af1b3baf8305aa9798dd5b39df70a
Hansen, Sivert Gullberg <sivert-gullberg.hansen@norsk-tipping.no>
2024-02-21 08:45:01 +0100

    remove old env

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import NextAuthSessionProvider from "@/providers/SessionProvider";
 import { Navbar } from "@/components/Navbar/Navbar";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
