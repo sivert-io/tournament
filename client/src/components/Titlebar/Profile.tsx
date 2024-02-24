f36fff6a24095135f9f140de8670ed7a3ad805d6
Sivert Gullberg Hansen <sivertgullberg@gmail.com>
2024-02-24 13:00:43 +0100

    Added player fetch info

--- client/src/components/Titlebar/Profile.tsx
+++ client/src/components/Titlebar/Profile.tsx
@@ -2,12 +2,16 @@ import { lang } from "@/lang";
 import { SteamProfile } from "next-auth-steam";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export function NavbarProfile({ steam }: { steam: SteamProfile }) {
   return (
     <div className="flex gap-2 text-sm font-medium items-center">
-      <div className="flex flex-col items-end text-xs">
+      <Link
+        href={`players/${steam.steamid}`}
+        className="flex flex-col items-end text-xs"
+      >
         <p className="font-normal truncate w-16 text-end">
           {steam.personaname}
         </p>
@@ -17,7 +21,7 @@ export function NavbarProfile({ steam }: { steam: SteamProfile }) {
         >
           {lang.user.sign_out}
         </button>
-      </div>
+      </Link>
       <Image
         src={steam.avatarfull}
         width={36}
