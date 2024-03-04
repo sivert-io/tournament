3afcf31b6cdf888452f0abfe1e27084d5fc86951
Hansen, Sivert Gullberg <sivert-gullberg.hansen@norsk-tipping.no>
2024-03-04 23:36:40 +0100

    Added shiny sign in CTA button

--- client/src/components/SignIn/SignIn.tsx
+++ client/src/components/SignIn/SignIn.tsx
@@ -1,11 +1,25 @@
 import { lang } from "@/lang";
+import { GradientButton } from "@lobehub/ui";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
 
+function handleClick() {
+  signIn("steam");
+}
+
 export function SignIn() {
+  // Should we show our cool glow button or the official steam login button?
+  const useOfficialButton =
+    Number(process.env.NEXT_PUBLIC_USE_OFFICIAL_BUTTON) || 1;
+  if (!!useOfficialButton)
+    return (
+      <GradientButton size="small" onClick={handleClick} glow={false}>
+        {lang.user.sign_in}
+      </GradientButton>
+    );
   return (
-    <button className="h-[35px] w-[180px]" onClick={() => signIn("steam")}>
+    <button className="h-[35px] w-[180px]" onClick={handleClick}>
       <Image
         alt={lang.user.sign_in}
         src="https://community.cloudflare.steamstatic.com/public/images/signinthroughsteam/sits_01.png"
